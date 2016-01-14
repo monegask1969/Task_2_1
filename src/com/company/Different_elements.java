5fd2d7575406fe1629fac448a2b1046c42cd100b
Max <monegask1969@gmail.com>
2016-01-14 17:06:36 +0200

    changes

---
+++ src/com/company/Different_elements.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * Created by Max on 09.01.2016.
+ */
+public class Different_elements {
+    public static void different_elements(int[] arr) {
+        char[] ch;
+        int min_povtor = 0;
+        int index_povtor = 0;
+        int povtor = 0;
+        String tmp;
+        for(int i=0; i<arr.length; i++){
+            tmp = Integer.toString(arr[i]);
+            ch = tmp.toCharArray();
+            for (int j=0; j<ch.length-1; j++) {
+                if (ch[j] == ch[j + 1]) {
+                    povtor +=1;
+                }
+                if(povtor < min_povtor){
+                    min_povtor = povtor;
+                    index_povtor = i;
+                }
+            }
+        }
+        System.out.println("The smallest number of repetitions in " + arr[index_povtor]);
+    }
+}
