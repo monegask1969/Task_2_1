7f809017b5f13ac77247ae4ac015f465f2aba6aa
Max <monegask1969@gmail.com>
2016-01-15 22:14:25 +0200

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 import java.util.Scanner;
 
+import static com.company.Different_elements.different_elements;
+import static com.company.Different_numbers.different_numbers;
+import static com.company.Even_numb_with_equal_oddeven.even_numb_equal;
+import static com.company.Increasing_numbers.increasing_numbers;
 import static com.company.More_less_medium_length.less_medium_length;
 import static com.company.Shortest_longest_number.longest;
 import static com.company.Shortest_longest_number.shortest;
@@ -24,6 +28,9 @@ public class Main {
        // sort_length_increase_value(arr); //вывод чисел в порядке возрастания
        // sort_length_decrease_value(arr); //вывод чисел в порядке убывания
        // less_medium_length(arr); // числа, длина которых меньше (больше) средней, а также длинa
-
+       // different_elements(arr); //число, в котором число различных цифр минимально
+      // even_numb_equal(arr);   //количество чисел, содержащих только четные цифры
+      //  increasing_numbers(arr); //число, цифры в котором идут в строгом порядке возрастания
+        different_numbers(arr);  //число, состоящее только из различных цифр
     }
 }
