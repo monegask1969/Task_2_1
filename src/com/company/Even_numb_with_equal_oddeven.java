b6ed3e4413e4ed0669882d9243418c2b1d1b79e8
Max <monegask1969@gmail.com>
2016-01-15 20:16:28 +0200

    changes

--- src/com/company/Even_numb_with_equal_oddeven.java
+++ src/com/company/Even_numb_with_equal_oddeven.java
@@ -10,6 +10,9 @@ public class Even_numb_with_equal_oddeven
         int kolvo = 0;
         char[] ch;
         String tmp;
+        int even = 0;
+        int odd = 0;
+        int equal = 0;
 
         for(int i=0; i<arr.length; i++)
         {
@@ -20,14 +23,22 @@ public class Even_numb_with_equal_oddeven
             {
                 if((ch[j] % 2 == 0) && (ch[j] != 0))
                 {
+                    even ++;
                     res = true;
                 }
+                else
+                    odd ++;
             }
             if(res == true)
             {
                 kolvo++;
             }
+            if(odd == even)
+            {
+             equal ++;
+            }
         }
         System.out.println("The amount of numbers with even numbers: " + kolvo);
+        System.out.println("The amount of numbers with equal number odd and even: " + equal);
     }
 }
