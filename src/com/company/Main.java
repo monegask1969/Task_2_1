a00cda198b61156f70d74ec986e1898780a2c193
Max <monegask1969@gmail.com>
2016-01-15 23:10:36 +0200

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ import static com.company.Different_numbers.different_numbers;
 import static com.company.Even_numb_with_equal_oddeven.even_numb_equal;
 import static com.company.Increasing_numbers.increasing_numbers;
 import static com.company.More_less_medium_length.less_medium_length;
+import static com.company.Numbers_k.numbers_k;
 import static com.company.Shortest_longest_number.longest;
 import static com.company.Shortest_longest_number.shortest;
 import static com.company.Sort_length_increase_value.sort_length_decrease_value;
@@ -15,6 +16,10 @@ import static com.company.Sort_length_increase_value.sort_length_increase_value;
 public class Main {
 
     public static void main(String[] args) {
+
+        numbers_k();    //Вывести числа от 1 до k в виде матрицы N x N слева направо и сверху вниз
+
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter quantity of numbers: ");
         int n = scan.nextInt();
@@ -31,6 +36,6 @@ public class Main {
        // different_elements(arr); //число, в котором число различных цифр минимально
       // even_numb_equal(arr);   //количество чисел, содержащих только четные цифры
       //  increasing_numbers(arr); //число, цифры в котором идут в строгом порядке возрастания
-        different_numbers(arr);  //число, состоящее только из различных цифр
+      //  different_numbers(arr);  //число, состоящее только из различных цифр
     }
 }
